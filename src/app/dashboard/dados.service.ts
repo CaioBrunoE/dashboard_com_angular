0ac1d79eee4f5fc491cf1a0e8b0fcf6f74049475
Caio Bruno <caiobruno90@yahoo.com>
2024-01-12 10:32:03 -0300

    finalizando dashboard

--- src/app/dashboard/dados.service.ts
+++ src/app/dashboard/dados.service.ts
@@ -14,18 +14,10 @@ export class DadosService {
     ];
 
 	constructor() {}
-
-	/**
-	 * Retorna um observable contendo os dados a serem
-	 * exibidos no gráfico.
-	 *
-	 * @return Observable<any>
-	 */
 	obterDados(): Observable<any> {
 		return new Observable(observable => {
 			observable.next(this.dados);
 			observable.complete();
 		});
 	}
-
 }
